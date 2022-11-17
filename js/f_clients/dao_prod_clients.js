123de14391a70b7868ab00e9dc3e76ec71a2bd1d
Gustavo Lozada <glozada@gestor.co>
2022-11-16 19:11:25 -0500

    4ETAPA: Pagina de promo navidad

---
+++ js/f_clients/dao_prod_clients.js
@@ -0,0 +1,26 @@
+import { collections } from "../persist/firebase_collections.js"
+import { dbRef } from "../persist/firebase_conexion.js"
+
+
+/**
+ * Buscar cliente por numero de identificacion
+ * @param {string} vsIdNumber Numero de identificacion
+ * @param {function} callback 
+ * @param {funcion} callbackError 
+ */
+export function findClientByIdNumber(vsIdNumber, callback, callbackError) {
+  dbRef.child(collections.clients).orderByChild("idNumber").equalTo(vsIdNumber)
+    .once('value')
+    .then(snap => {
+      console.log("findByIdNumber exist=", snap.exists())
+      if (snap.exists()) {
+        snap.forEach(child => {
+          let cli = child.val()
+          callback(vsIdNumber, cli.stRaffleCupons)
+        })
+      } else {
+        callback(vsIdNumber)
+      }
+    })
+    .catch(error => callbackError(vsIdNumber, error))
+}
---
+++ js/f_promos/manager_promo_navidad.js
@@ -0,0 +1,50 @@
+import { findClientByIdNumber } from "../f_clients/dao_prod_clients.js"
+
+const d = document,
+  w = window
+
+//------------------------------------------------------------------------------------------------
+// Delegacion de eventos
+//------------------------------------------------------------------------------------------------
+
+// EVENTO=load RAIZ=window 
+w.addEventListener("load", () => console.log("Available Screen Width: " + screen.availWidth + ", Height: " + screen.availHeight))
+
+// EVENTO=change RAIZ=button<search> ACCION=Realizar busqueda
+d.addEventListener("submit", e => {
+  e.preventDefault()
+  const idClient = d.querySelector(".identification").value
+  findClientByIdNumber(idClient,
+    (idClient, vsRaffleCupons) => renderExpense(idClient, vsRaffleCupons),
+    (idClient, error) => {
+      console.log("Errro en consulta de cupones", error)
+      renderExpense(idClient)
+    })
+})
+
+
+function renderExpense(vsIdClient, vsRaffleCupons) {
+
+  let $raffleCupons = d.querySelector(".raffle-cupons")
+  let vaCupons = vsRaffleCupons && vsRaffleCupons.length > 1 ? vsRaffleCupons.split(" ") : []
+
+  $raffleCupons.classList.remove("is-hidden")
+
+  if (vaCupons.length > 0) {
+    const $fragment = d.createDocumentFragment()
+    vaCupons.forEach(numberCupon => {
+      let $cupon = d.getElementById("cupon").content.cloneNode(true)
+      $cupon.querySelector(".cupon-number").innerText = numberCupon
+      $fragment.appendChild($cupon)
+    })
+    $raffleCupons.innerHTML = ""
+    $raffleCupons.appendChild($fragment)
+  } else {
+    $raffleCupons.innerHTML = `<div class="column is-expanded">
+    <p class="has-text-white is-size-4">
+      Aún no tienes cupones😔, pero te esperamos en <b>ELEGANCE</b> STUDIO para cambiar tu suerte😉
+    </p>
+  </div>`
+  }
+
+}
